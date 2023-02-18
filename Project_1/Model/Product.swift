aac6793f804933bf43a0034c943f6fb1e102efa0
Vahe Sargsyan <vahesargsyan@Vahes-MacBook-Pro.local>
2023-02-18 02:13:17 -0800

    add project 1

---
+++ Project_1/Model/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  Project_1
+//
+//  Created by Valodya Galstyan on 11.02.23.
+//
+
+import Foundation
+
+struct Product {
+    var icon: String
+    var title: String
+    var price: Float
+    var isFavorite: Bool
+}
